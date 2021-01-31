d36764fc8d1ce15fbab6d85e414b49c05a59d317
ASO1801012 <1801012@s.asojuku.ac.jp>
2021-01-31 19:19:01 +0900

    Initial commit

---
+++ app/Thread.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    //
+    protected $table = 'threads';
+    protected $primaryKey = 'id';
+    protected $guarded = ['id'];
+
+    public function comment(){
+        return $this->hasmany('App\Comment','thread_id');
+    }
+
+    public function tag(){
+        return $this->belongsTo('App\tag');
+    }
+}
